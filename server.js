3b8f1f783de4c38b7ce2aabfba7404207eb3c1b8
Jered <jered@Jereds-MacBook-Pro.local>
2022-09-02 21:56:00 -0700

    setting up server with mongodb connection

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 import express from "express";
 const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+
+import connectDB from "./db/connect.js";
 
 //middleware
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 
 app.get("/", (req, res) => {
-  throw new Error("error");
   res.send("Welcome!");
 });
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
-
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}...`);
 });
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {}
+};
+
+start();
