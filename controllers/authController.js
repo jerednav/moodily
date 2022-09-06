edb45ab70188337d0df0f2c5a42bb9796f64e88d
Jered <jered@Jereds-MacBook-Pro.local>
2022-09-05 19:14:14 -0700

    set up express async errors and status codes dependencies

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,10 @@
 import User from "../models/User.js";
+import { StatusCodes } from "http-status-codes";
 
 //next argument will pass the error to the middleware
-const register = async (req, res, next) => {
-  try {
-    const user = await User.create(req.body);
-    res.status(201).json({ user });
-  } catch (error) {
-    next(error);
-  }
+const register = async (req, res) => {
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
 };
 const login = async (req, res) => {
   res.send("login user");
