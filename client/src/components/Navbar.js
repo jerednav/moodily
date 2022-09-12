91779e2c0b5dbd860ca83dd0febd878166cc6d62
Jered <jered@Jereds-MacBook-Pro.local>
2022-09-11 22:10:59 -0700

    navbar structure

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,44 @@
 import Wrapper from "../assets/wrappers/Navbar";
-import { FaHome } from "react-icons/fa";
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import { useAppContext } from "../context/appContext";
+import Logo from "./Logo";
 
 const Navbar = () => {
   return (
     <Wrapper>
-      <h4>Navbar</h4>
-      <FaHome />
+      <div class="nav-center">
+        <button
+          type="button"
+          class="toggle-btn"
+          onClick={() => console.log("toggle sidebar")}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 class="logo-text">dashboard</h3>
+        </div>
+        <div class="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => console.log("show/hide dropdown")}
+          >
+            <FaUserCircle />
+            jered
+            <FaCaretDown />
+          </button>
+          <div class="dropdown show-dropdown">
+            <button
+              type="button"
+              className="dropdown-btn"
+              onClick={() => console.log("logout")}
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
