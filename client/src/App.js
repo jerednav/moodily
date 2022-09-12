4c4e541a6a2ac274d121d0603df4bf04ae466463
Jered <jered@Jereds-MacBook-Pro.local>
2022-09-11 21:43:08 -0700

    protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Register, Error, Landing } from "./pages";
+import { Register, Error, Landing, ProtectedRoute } from "./pages";
 import {
   AllMoods,
   AddMood,
@@ -12,7 +12,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
           <Route index element={<Stats />} />
           <Route path="all-moods" element={<AllMoods />} />
           <Route path="add-mood" element={<AddMood />} />
