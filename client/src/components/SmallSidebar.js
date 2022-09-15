a3c542965874d48a7894a90ef5f05cf7eb727c33
Jered <jered@Jereds-MacBook-Pro.local>
2022-09-14 21:26:36 -0700

    set up small sidebar

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,8 +1,7 @@
 import Wrapper from "../assets/wrappers/SmallSidebar";
 import { FaTimes } from "react-icons/fa";
 import { useAppContext } from "../context/appContext";
-import links from "../utils/links";
-import { NavLink } from "react-router-dom";
+import NavLinks from "./NavLinks";
 import Logo from "./Logo";
 
 export const SmallSidebar = () => {
@@ -21,25 +20,7 @@ export const SmallSidebar = () => {
           <header>
             <Logo />
           </header>
-          <div className="nav-links">
-            {links.map((link) => {
-              const { text, path, id, icon } = link;
-
-              return (
-                <NavLink
-                  to={path}
-                  key={id}
-                  onClick={toggleSidebar}
-                  className={({ isActive }) =>
-                    isActive ? "nav-link active" : "nav-link"
-                  }
-                >
-                  <span className="icon">{icon}</span>
-                  {text}
-                </NavLink>
-              );
-            })}
-          </div>
+          <NavLinks toggleSidebar={toggleSidebar} />
         </div>
       </div>
     </Wrapper>
