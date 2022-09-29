5a185ba5666aaab2cdd594f075d195bbe04778b7
Jered <jered@Jereds-MacBook-Pro.local>
2022-09-28 21:55:42 -0700

    adding checking permissions

--- controllers/moodsController.js
+++ controllers/moodsController.js
@@ -1,6 +1,11 @@
 import Mood from "../models/Mood.js";
 import { StatusCodes } from "http-status-codes";
-import { BadRequestError, UnAuthenticatedError } from "../errors/index.js";
+import {
+  BadRequestError,
+  NotFoundError,
+  UnAuthenticatedError,
+} from "../errors/index.js";
+import checkPermissions from "../utils/checkPermissions.js";
 
 const createMood = async (req, res) => {
   const { currentMood } = req.body;
@@ -21,8 +26,31 @@ const getAllMoods = async (req, res) => {
     .json({ moods, totalMoods: moods.length, numOfPages: 1 });
 };
 const updateMood = async (req, res) => {
-  res.send("update mood");
+  const { id: moodId } = req.params;
+  const { currentMood, moodLocation } = req.body;
+
+  if (!currentMood || !moodLocation) {
+    throw new BadRequestError("Please provide all values");
+  }
+  const mood = await Mood.findOne({ _id: moodId });
+
+  if (!mood) {
+    throw new NotFoundError(`No mood with id :${moodId}`);
+  }
+
+  //check permissions
+  // console.log(typeof req.user.userId);
+  // console.log(typeof mood.createdBy);
+
+  checkPermissions(req.user, mood.createdBy);
+
+  const updatedMood = await Mood.findOneAndUpdate({ _id: moodId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(StatusCodes.OK).json({ updatedMood });
 };
+
 const deleteMood = async (req, res) => {
   res.send("delete mood");
 };
